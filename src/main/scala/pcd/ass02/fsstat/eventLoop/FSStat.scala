6169e3d3daaaa266c7c9471aea72ebdfa4992810
Pietro Pasini <pietro.pasini4@studio.unibo.it>
2026-04-23 20:13:47 +0200

    make more robust, from object to class

--- src/main/scala/pcd/ass02/fsstat/eventLoop/FSStat.scala
+++ src/main/scala/pcd/ass02/fsstat/eventLoop/FSStat.scala
@@ -1,42 +1,54 @@
 package pcd.ass02.fsstat.eventLoop
 
 import io.vertx.core.*
-import eventbus.{EventBus, Message}
+import eventbus.*
 import file.*
 import scala.util.Random
 
-object FSStat:
-  val vertx: Vertx = Vertx.vertx()
-  val eb: EventBus = vertx.eventBus()
-  val fs: FileSystem = vertx.fileSystem()
+class FSStat extends AbstractVerticle:
+  var eb: EventBus = _
+  var fs: FileSystem = _
+  var logError = true
+
+  override def start(): Unit =
+    eb = vertx.eventBus()
+    fs = vertx.fileSystem()
 
   def getFSReport(D: String, maxFS: Int, NB: Int): Future[(Int, Array[Long])] =
-    val counts = Array.ofDim[Long](NB + 1)
-    val steps = Range(1, NB + 1).map(i => (i * maxFS) / NB).zipWithIndex
-    var total = 0
-    val p = Promise.promise[(Int, Array[Long])]()
-    val topic = Random().nextInt().toString
+    if eb == null || fs == null then
+      Future.failedFuture(new IllegalStateException("Verticle not started"))
+    if maxFS < NB || NB <= 0 then
+      Future.failedFuture(new IllegalArgumentException("maxFS must be >= NB, NB must be positive"))
+    else
+      val counts = Array.ofDim[Long](NB + 1)
+      val steps = Range(1, NB + 1).map(i => (i * maxFS) / NB).zipWithIndex
+      var total = 0
+      val p = Promise.promise[(Int, Array[Long])]()
+      val topic = Random().nextInt().toString
 
-    eb.consumer[Long](topic, msg =>
-      counts(steps.find(t => msg.body() < t._1).map(o => o._2).getOrElse(NB)) += 1
-      total += 1
-    )
-    eb.consumer[Unit](topic+"f", msg =>
-      p.complete((total, counts))
-      vertx.close()
-    )
-    vertx.deployVerticle(new FSReader(D, topic))
-    p.future()
+      eb.consumer[Long](topic, msg =>
+        counts(steps.find(t => msg.body() < t._1).map(o => o._2).getOrElse(NB)) += 1
+        total += 1
+      )
+      eb.consumer[Unit](topic+"f", msg => p.complete((total, counts)))
+      vertx.deployVerticle(new FSReader(D, topic))
+      p.future()
 
   private class FSReader(dir: String, topic: String) extends AbstractVerticle:
     override def start(): Unit =
-      scanPath(dir).onComplete(res => eb.publish(topic+"f", ()))
+      scanPath(dir).onComplete(_ =>
+        eb.publish(topic+"f", ())
+        vertx.undeploy(deploymentID())
+      )
 
     private def scanPath(path: String): Future[Unit] =
       fs.props(path).compose(props =>
         if props.isDirectory then
-          fs.readDir(path).compose(paths => Future.join(paths.stream().map(scanPath).toList).map(_ => ()))
+          fs.readDir(path).compose(paths => Future.all(paths.stream().map(scanPath).toList).map(_ => ()))
         else
           eb.publish(topic, props.size())
           Future.succeededFuture()
+      ).recover(err =>
+        if logError then println("Error: " + err.getMessage)
+        Future.succeededFuture()
       )
--- src/test/scala/pcd/ass02/fsstat/eventLoop/TestFSStat.scala
+++ src/test/scala/pcd/ass02/fsstat/eventLoop/TestFSStat.scala
@@ -1,21 +1,29 @@
 package pcd.ass02.fsstat.eventLoop
 
+import io.vertx.core.Vertx
+
 object TestFSStat:
   def main(args: Array[String]): Unit =
     val D = "."
     val maxFS = 1000
     val NB = 10
     val startTime = System.currentTimeMillis()
-    FSStat.getFSReport(D, maxFS, NB).onSuccess((total, counts) =>
-      val endTime = System.currentTimeMillis()
-      println(s"Time taken: ${endTime - startTime} ms")
-      println(s"Total files: $total")
-      println("Counts per size range:")
-      counts.zipWithIndex.foreach((count, index) =>
-        val rangeStart = if index == 0 then 0 else (index * maxFS) / NB + 1
-        val rangeEnd = ((index + 1) * maxFS) / NB
-        println(s"Size <= $rangeEnd bytes: $count")
+    val fsStat = new FSStat
+    val vertx = Vertx.vertx()
+    vertx.deployVerticle(fsStat).onComplete(_ =>
+      fsStat.getFSReport(D, maxFS, NB).onSuccess((total, counts) =>
+        val endTime = System.currentTimeMillis()
+        println(s"Time taken: ${endTime - startTime} ms")
+        println(s"Total files: $total")
+        println("Counts per size range:")
+        counts.zipWithIndex.foreach((count, index) =>
+          val rangeStart = if index == 0 then 0 else (index * maxFS) / NB + 1
+          val rangeEnd = ((index + 1) * maxFS) / NB
+          println(s"Size <= $rangeEnd bytes: $count")
+        )
+      ).onFailure(err =>
+        println(s"Error: ${err.getMessage}")
+      ).onComplete(_ =>
+        vertx.close()
       )
-    ).onFailure(err =>
-      println(s"Error: ${err.getMessage}")
     )
